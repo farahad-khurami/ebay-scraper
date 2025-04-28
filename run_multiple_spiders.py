33c57a2c8b17838f620a3c123e1cf331121cab39
Farahad Khurami <fkhurami98@gmail.com>
2025-04-28 18:43:29 +0100

    Add run_multiple_spiders.py file

---
+++ run_multiple_spiders.py
@@ -0,0 +1,23 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+def run_spiders():
+    """
+    Runs multiple instances of the eBay spider with different arguments.
+    """
+    process = CrawlerProcess(get_project_settings())
+
+    instances = [
+        {"search_query": "size 9 nikes", "max_items": 1000},
+        {"search_query": "rolex", "max_items": 1000},
+        {"search_query": "bionicle", "max_items": 1000},
+    ]
+
+    for instance in instances:
+        process.crawl("ebay_sold_items", **instance)
+
+    # Start the crawling process
+    process.start()
+
+if __name__ == "__main__":
+    run_spiders()
